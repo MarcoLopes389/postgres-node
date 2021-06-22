57dd11ce1e89b172834a548a235bf646d8f99918
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-06-22 17:58:03 -0300

    CRUD finalizado

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(cors())
 const data = new Date()
 
 app.post('/add', async (req, res) => {
-    const { user, email } = req.body
+    const { user, email, senha } = req.body
     const client = await db.connect()
-    await client.query('insert into users("name","email") values($1, $2)', [user, email])
+    await client.query('insert into users("name","email", "password") values($1, $2, $3)', [user, email, senha])
     const resultado = await client.query('select * from users')
     return res.json(resultado.rows)
 })
@@ -22,10 +22,13 @@ app.get('/list', async (req, res) => {
     return res.json(resultado.rows)
 })
 
-app.delete('/deleteall', async (req, res) => {
+app.delete('/delete', async (req, res) => {
+
+    const { email, senha } = req.body
+
     const client = await db.connect()
     try {
-        await client.query("delete from users where email= 'nenhum@gmail.com'")
+        await client.query("delete from users where email = $1 and password = $2", [email, senha])
         let ok = true
         return res.json({
             ok: ok
@@ -36,6 +39,16 @@ app.delete('/deleteall', async (req, res) => {
             ok: ok
         })
     }
+})
+
+app.patch('/update', async (req, res) => {
+
+    const { id, senha, email } = req.body
+
+    const client = await db.connect()
+    await client.query('update users set password = $1, email= $2 WHERE id = $3', [senha, email, id])
+    return res.json(resultado.rows)
+
 })
 
 app.listen(3000, () => {
