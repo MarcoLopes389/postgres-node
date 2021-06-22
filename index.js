2a847885acf1f0ff9711f66a9a211d7efb573689
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-06-22 15:26:38 -0300

    Iniciadas as primeiras funções para o postgres

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const db = require('./config/database')
+
+app.use(express.json())
+app.use(cors())
+
+const data = new Date()
+
+app.post('/add', async (req, res) => {
+    const { user, email } = req.body
+    await db.connect()
+    await db.query('insert into users("name","email") values($1, $2)', [user, email])
+    const resultado = await db.query('select * from users')
+    await db.end()
+    return res.json(resultado.rows)
+})
+
+app.get('/list', async (req, res) => {
+    await db.connect()
+    const resultado = await db.query('select * from users')
+    await db.end()
+    return res.json(resultado.rows)
+})
 
 app.listen(3000, () => {
     console.log('Servidor rodando...')
