eed0646e3577256e58454b8d06e178313679e559
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-06-24 15:26:15 -0300

    Finalizados o crud de dados em texto

--- index.js
+++ index.js
@@ -10,13 +10,22 @@ const data = new Date()
 
 app.post('/add', async (req, res) => {
     const { user, email, senha } = req.body
-    const client = await db.connect()
-    await client.query('insert into users("name","email", "password") values($1, $2, $3)', [user, email, senha])
-    const resultado = await client.query('select * from users')
-    return res.json(resultado.rows)
+
+    try {
+        const client = await db.connect()
+        await client.query('insert into users("name","email", "password") values($1, $2, $3)', [user, email, senha])
+        const resultado = await client.query('select * from users')
+        return res.json(resultado.rows)
+    } catch {
+        return res.json({
+            status: 401,
+            err: 'Esse usuário já existe'
+        })
+    }
 })
 
 app.get('/list', async (req, res) => {
+
     const client = await db.connect()
     const resultado = await client.query('select * from users')
     return res.json(resultado.rows)
@@ -24,11 +33,11 @@ app.get('/list', async (req, res) => {
 
 app.delete('/delete', async (req, res) => {
 
-    const { email, senha } = req.body
+    const { id } = req.body
 
     const client = await db.connect()
     try {
-        await client.query("delete from users where email = $1 and password = $2", [email, senha])
+        await client.query("delete from users where id = $1", [id])
         let ok = true
         return res.json({
             ok: ok
@@ -46,7 +55,7 @@ app.patch('/update', async (req, res) => {
     const { id, senha, email } = req.body
 
     const client = await db.connect()
-    await client.query('update users set password = $1, email= $2 WHERE id = $3', [senha, email, id])
+    const resultado = await client.query('update users set password = $1, email= $2 WHERE id = $3', [senha, email, id])
     return res.json(resultado.rows)
 
 })
