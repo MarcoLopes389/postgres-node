2140375acd9db8f0fbe7ae010cc3155c41392b9e
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-07-05 11:24:03 -0300

    Retirando funcionalidade de envio de ícone desnecessário

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ const data = new Date()
 
 app.post('/add', async (req, res) => {
     const { user, email, senha } = req.body
-    const { name, data } = req.files.icon
 
     try {
         console.log(data)
         const client = await db.connect()
-        await client.query('insert into users("name","email", "password", "icon") values($1, $2, $3, $4)', [user, email, senha, data])
+        await client.query('insert into users("name","email", "password") values($1, $2, $3, $4)', [user, email, senha])
         const resultado = await client.query('select * from users')
         return res.json(resultado.rows)
 
@@ -67,16 +66,6 @@ app.patch('/update', async (req, res) => {
     const client = await db.connect()
     const resultado = await client.query('update users set password = $1, email= $2 WHERE id = $3', [senha, email, id])
     return res.json(resultado.rows)
-
-})
-
-app.get('/img/:id', async (req, res) => {
-
-    const client = await db.connect()
-    const resposta = await client.query('select icon from users where id = $1', [req.params.id])
-    const img = await createReadStream(resposta.rows[0].icon)
-    const read = new Readable(img)
-    await read.pipe(res)
 
 })
 
