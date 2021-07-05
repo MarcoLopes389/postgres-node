535d825e903d03e8a60ada60d76b35f981fdeade
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-07-05 11:13:20 -0300

    Retirando código que não funciona

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const { Readable } = require('stream')
+
 const app = express()
 const db = require('./config/database')
+const { createReadStream } = require('fs')
 
+app.use(fileUpload())
 app.use(express.json())
 app.use(cors())
 
@@ -10,16 +15,21 @@ const data = new Date()
 
 app.post('/add', async (req, res) => {
     const { user, email, senha } = req.body
+    const { name, data } = req.files.icon
 
     try {
+        console.log(data)
         const client = await db.connect()
-        await client.query('insert into users("name","email", "password") values($1, $2, $3)', [user, email, senha])
+        await client.query('insert into users("name","email", "password", "icon") values($1, $2, $3, $4)', [user, email, senha, data])
         const resultado = await client.query('select * from users')
         return res.json(resultado.rows)
-    } catch {
+
+    } catch (err) {
+
+        console.log(err)
         return res.json({
             status: 401,
-            err: 'Esse usuário já existe'
+            err: err
         })
     }
 })
@@ -57,6 +67,16 @@ app.patch('/update', async (req, res) => {
     const client = await db.connect()
     const resultado = await client.query('update users set password = $1, email= $2 WHERE id = $3', [senha, email, id])
     return res.json(resultado.rows)
+
+})
+
+app.get('/img/:id', async (req, res) => {
+
+    const client = await db.connect()
+    const resposta = await client.query('select icon from users where id = $1', [req.params.id])
+    const img = await createReadStream(resposta.rows[0].icon)
+    const read = new Readable(img)
+    await read.pipe(res)
 
 })
 
